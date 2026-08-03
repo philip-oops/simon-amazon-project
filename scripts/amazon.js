658b289f591b8aa0edb1cec292372832ae3efe3e
philip <victoriaokwudili714@gmail.com>
2026-08-03 20:23:34 +0100

    use products.js file

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,34 +1,6 @@
 const jsProductGrid = document.querySelector('.js-products-grid')
 
 
-const products = [{
-    image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-    name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-    rating: {
-        stars: 4.5,
-        count: 87
-    },
-    priceCents: 1090
-},{
-    image: 'images/products/intermediate-composite-basketball.jpg',
-    name: 'Intermediate Size Basketball',
-    rating: {
-        stars: 4,
-        count: 127
-    },
-    priceCents: 2095
-},{
-    image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-    name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-    rating: {
-        stars: 4.5,
-        count: 56
-    },
-    priceCents: 799
-}
-]; 
-
-// combining all the string together
 let productsHtml = '';
 
 // used accumulator pattern
@@ -85,6 +57,4 @@ products.forEach((product) => {
     `;
     
 })
-
-console.log(productsHtml);
 jsProductGrid.innerHTML = productsHtml 
