3df22bd29ac9921fc4cc54f3d510be0673f7537e
philip <victoriaokwudili714@gmail.com>
2026-08-13 21:22:50 +0100

    make payment summary interactive

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -3,6 +3,7 @@ import { products, getProduct } from '../../data/products.js';
 import { formatCurrency } from '../utils/money.js';
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 import { deliveryOptions, getDeliveryOption } from '../../data/deliveryOptions.js';
+import { renderPaymentSummary } from './paymentSummary.js';
 
 
 export function renderOrderSummary() {
@@ -117,6 +118,8 @@ cart.forEach((cartItem) => {
 
       container.remove();
 
+      renderPaymentSummary();
+
     });
 
   });
@@ -138,6 +141,7 @@ cart.forEach((cartItem) => {
       // Re-render the whole order summary
       // so the delivery date changes immediately
       renderOrderSummary();
+      renderPaymentSummary();
 
     });
 
