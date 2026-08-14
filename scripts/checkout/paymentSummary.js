a47db11a0a3b171e7a6e30b1c8ed3f71ce7949ab
philip <victoriaokwudili714@gmail.com>
2026-08-14 08:36:43 +0100

    create automated test for format currency

--- scripts/checkout/paymentSummary.js
+++ scripts/checkout/paymentSummary.js
@@ -6,6 +6,7 @@ import { formatCurrency } from "../utils/money.js";
 export function renderPaymentSummary() {
     let productPriceCents = 0;
     let shippingPriceCents = 0;
+    let itemCount = 0;
 
     cart.forEach((cartItem) => {
         const product = getProduct(cartItem.productId);
@@ -13,6 +14,7 @@ export function renderPaymentSummary() {
 
         const deliveryOption = getDeliveryOption(cartItem.deliveryOptionId);
         shippingPriceCents += deliveryOption.priceCents
+        itemCount += cartItem.quantity;
     });
 
     const totalBeforeTaxCents = productPriceCents + shippingPriceCents;
@@ -25,7 +27,7 @@ export function renderPaymentSummary() {
           </div>
 
           <div class="payment-summary-row">
-            <div>Items (3):</div>
+            <div>Items  (<span class="js-items-number">${itemCount}</span>):</div>
             <div class="payment-summary-money">$${formatCurrency(productPriceCents)}</div>
           </div>
 
